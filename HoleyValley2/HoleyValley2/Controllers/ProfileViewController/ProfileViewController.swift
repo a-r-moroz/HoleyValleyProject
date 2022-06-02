a3a6bb91a301941617d16d42e86abe2e7a91fed3
user-puser-1 <andrew101520@gmail.com>
2022-06-03 00:53:20 +0300

    created a skeleton of ProfileVC

--- HoleyValley2/HoleyValley2/Controllers/ProfileViewController/ProfileViewController.swift
+++ HoleyValley2/HoleyValley2/Controllers/ProfileViewController/ProfileViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = "Профиль"
     }
 
 
