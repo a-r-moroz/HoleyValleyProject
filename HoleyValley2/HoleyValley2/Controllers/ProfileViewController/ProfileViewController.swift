d2581298516db65d247cfb4123dbe10c786f9d6c
user-puser-1 <andrew101520@gmail.com>
2022-06-09 13:12:53 +0300

    added emerging of view with textFields on ProfileVC

--- HoleyValley2/HoleyValley2/Controllers/ProfileViewController/ProfileViewController.swift
+++ HoleyValley2/HoleyValley2/Controllers/ProfileViewController/ProfileViewController.swift
@@ -9,22 +9,54 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var editButtonOutlet: UIButton!
+    @IBOutlet weak var nameInputField: InputField!
+    @IBOutlet weak var surnameInputField: InputField!
+    @IBOutlet weak var phoneInputField: InputField!
+    @IBOutlet weak var viewWithData: UIView!
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
 
         title = "Профиль"
+        viewWithData.setRoundingToView(cornerRadius: Const.CornerRadiusTo.viewAndImage)
+        nameInputField.validationType = .name
+        nameInputField.textField.placeholder = "Имя"
+        surnameInputField.validationType = .surname
+        surnameInputField.textField.placeholder = "Фамилия"
+        phoneInputField.validationType = .phone
+        phoneInputField.textField.placeholder = "+375 (XX) XXX-XX-XX"
+        phoneInputField.textField.keyboardType = .numberPad
+//        phoneInputField.textField.delegate = self
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func editAction(_ sender: UIButton) {
+        
+        UIView.animate(withDuration: 0.3, delay: 0.1) {
+            
+            self.nameLabel.isHidden = true
+            self.phoneLabel.isHidden = true
+            self.editButtonOutlet.isHidden = true
+            self.viewWithData.isHidden = false
+        }
     }
-    */
 
+    @IBAction func saveAction(_ sender: UIButton) {
+        
+        
+    }
+    
+    @IBAction func cancelAction(_ sender: UIButton) {
+        
+        UIView.animate(withDuration: 0.3, delay: 0.1) {
+            
+            self.viewWithData.isHidden = true
+            self.nameLabel.isHidden = false
+            self.phoneLabel.isHidden = false
+            self.editButtonOutlet.isHidden = false
+        }
+    }
 }
