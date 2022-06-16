bcee66138421679dc21cacc2ab185b91e225156e
a-r-moroz <andrew101520@gmail.com>
2022-06-17 00:28:46 +0300

    added text animation on splashVC

--- HoleyValley2/HoleyValley2/Controllers/SplashViewController/SplashViewController.swift
+++ HoleyValley2/HoleyValley2/Controllers/SplashViewController/SplashViewController.swift
@@ -10,19 +10,30 @@ import UIKit
 final class SplashViewController: UIViewController {
 
     @IBOutlet weak var gradientView: UIImageView!
-    @IBOutlet weak var oldConstraint: NSLayoutConstraint!
-    @IBOutlet weak var newConstraint: NSLayoutConstraint!
+    @IBOutlet weak var oldGradientConstraint: NSLayoutConstraint!
+    @IBOutlet weak var oldTextConstraint: NSLayoutConstraint!
+    @IBOutlet weak var newGradientConstraint: NSLayoutConstraint!
+    @IBOutlet weak var newTextConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.oldConstraint.isActive = false
-        self.newConstraint.isActive = true
+        self.oldGradientConstraint.isActive = false
+        self.newGradientConstraint.isActive = true
         
         UIView.animate(withDuration: 1.5, delay: 0.2, options: [.curveEaseInOut]) {
             self.view.layoutIfNeeded()
         } completion: { finish in
             
+        }
+        
+        self.oldTextConstraint.isActive = false
+        self.newTextConstraint.isActive = true
+
+        UIView.animate(withDuration: 1.0, delay: 0.5, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: [.curveEaseOut]) {
+            
+            self.view.layoutIfNeeded()
+
         }
         
         
