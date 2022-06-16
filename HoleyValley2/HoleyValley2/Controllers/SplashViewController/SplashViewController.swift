05a2b60a954966828f56e864c03cf97826bfa67f
a-r-moroz <andrew101520@gmail.com>
2022-06-17 00:16:42 +0300

    assembled splashVC
    with animation

--- HoleyValley2/HoleyValley2/Controllers/SplashViewController/SplashViewController.swift
+++ HoleyValley2/HoleyValley2/Controllers/SplashViewController/SplashViewController.swift
@@ -19,16 +19,18 @@ final class SplashViewController: UIViewController {
         self.oldConstraint.isActive = false
         self.newConstraint.isActive = true
         
-        UIView.animate(withDuration: 2.0, delay: 0.0, options: [.curveEaseInOut]) {
+        UIView.animate(withDuration: 1.5, delay: 0.2, options: [.curveEaseInOut]) {
             self.view.layoutIfNeeded()
         } completion: { finish in
             
+        }
+        
+        
 //            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
 //            let sceneDelegate = windowScene.delegate as? SceneDelegate
 //            else { return }
 //
 //            sceneDelegate.splashController()
-        }
 
         
 //        UIView.animate(withDuration: 2.0) {
